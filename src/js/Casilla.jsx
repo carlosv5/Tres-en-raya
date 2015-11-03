463793460d53582fdb6f7608b7b3187546315d76
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-11-03 17:58:14 +0100

    Estructura y estado añadido

---
+++ src/js/Casilla.jsx
@@ -0,0 +1,19 @@
+const casillaStyle = {
+  height: '100px',
+  width: '100px'
+};
+let Casilla = React.createClass({
+  getInitialState: function(){
+    return {
+      valor: 'X'
+    }
+  },
+  render: function() {
+    return (
+      <button style={casillaStyle}>
+      {this.state.valor}
+      </button>
+    );
+  }
+});
+module.exports = Casilla;
