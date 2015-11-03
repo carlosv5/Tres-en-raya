c1ec75f1ba98cd3ab37f14a4e3b9c71de4327f5f
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-11-03 16:41:21 +0100

    Primer componente: Casilla

---
+++ src/js/index.jsx
@@ -0,0 +1,11 @@
+var Casilla = React.createClass({
+  render: function() {
+    return (
+      <div>
+      {this.props.valor}
+      </div>
+    );
+  }
+});
+ReactDOM.render(<Casilla valor="X" />,
+document.getElementById('contenedor'));
